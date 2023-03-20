bb38ceaa87bcf4ec542ff22e97ed7a5fa8612772
Oleksandra <alexadrobot@gmail.com>
2023-03-20 19:55:59 +0200

    2/3

--- app/src/main/java/com/example/laboratorna2/MainActivity.kt
+++ app/src/main/java/com/example/laboratorna2/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.laboratorna2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.example.laboratorna2.databinding.ActivityMainBinding
 import com.example.laboratorna2.databinding.ActivitySecondBinding
 
@@ -17,10 +19,21 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivityForResult(intent, 1)
         }
+        binding.buttonToEnter.setOnClickListener {
+            if (binding.enterLogin.text.toString() == "Shevchenko" && binding.enterPassword.text.toString() == "1814"){
+                binding.LayoutToEnter.isVisible = false
+            }
+
+            else
+                binding.textToError.text = "Wrong password!"
+        }
     }
+    @SuppressLint("SuspiciousIndentation")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == 1)
         binding.textName.text = data?.getStringExtra("username")
     }
+
+
 }
