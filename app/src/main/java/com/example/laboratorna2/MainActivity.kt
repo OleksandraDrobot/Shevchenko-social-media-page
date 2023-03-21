7b84961ff1cea21ed663b780974b0e0eedad38c0
Oleksandra <alexadrobot@gmail.com>
2023-03-21 15:48:23 +0200

    3/3

--- app/src/main/java/com/example/laboratorna2/MainActivity.kt
+++ app/src/main/java/com/example/laboratorna2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.laboratorna2
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isVisible
@@ -10,6 +11,7 @@ import com.example.laboratorna2.databinding.ActivitySecondBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    val SELECT_PICTURE = 111
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,13 +29,35 @@ class MainActivity : AppCompatActivity() {
             else
                 binding.textToError.text = "Wrong password!"
         }
+
+        binding.mainPhoto.setOnClickListener {
+            imageChooser();
+        }
     }
+
+    fun imageChooser() {
+        val i = Intent()
+        i.type = "image/*"
+        i.action = Intent.ACTION_GET_CONTENT
+
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE)
+    }
+
+
     @SuppressLint("SuspiciousIndentation")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == 1)
-        binding.textName.text = data?.getStringExtra("username")
-    }
+        if (resultCode == 1) {
+            binding.textName.text = data?.getStringExtra("username")
 
+        }
+        if (requestCode == SELECT_PICTURE) {
+            val selectedImageUri: Uri? = data?.data
+            if (null != selectedImageUri) {
+                // update the preview image in the layout
+                binding.mainPhoto.setImageURI(selectedImageUri)
+            }
+        }
+    }
 
 }
